fcdbc2b9d6475915e1b11162691b20dc4d45da59
silma01 <cscbgtt@gmail.com>
2026-07-31 08:39:14 +0700

    Add files via upload

---
+++ index_fix.php
@@ -0,0 +1,6 @@
+<?php
+$nama = "Ridea Ariyanto";
+$age = "30";
+echo "Halo nama saya : $nama/n";
+echo "Umur saya : $age tahun/n";
+?>
---
+++ string_dan_variable.php
@@ -0,0 +1,18 @@
+<?php
+
+// --- 1. Variable & Double Quote ---
+$nama = "eaeaeaea";
+$age = 30;
+
+echo "Nama : ";
+echo 'Ridea ' . $nama; // Pakai single quote & gabung string
+echo "\n";
+
+echo "Name : $nama\n";
+echo "Age : $age\n";
+
+// --- 2. Single Quote ---
+echo 'Nama : ';
+echo 'Ridea Ariyanto';
+echo "\n";
+?>
